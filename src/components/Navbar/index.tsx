ec7c12d9c7b2a0d6d82864f8f8fa266943edbfb7
Victor Gonzalez <vitorpcg@gmail.com>
2024-01-03 15:47:09 +0000

    ajusting
    ajusting

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,28 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className='navbar navbar-expand-md navbar bg-dark'>
+    <nav className='navbar navbar-expand-md d-flex justify-content-center'>
       <div className='navbar-nav mr-auto d-flex gap-4'>
-        <NavLink className='btn btn-primary bg-dark border' to='/'>Home</NavLink>
+        <NavLink to='/'></NavLink>
         <div className="btn-group">
-          <button type="button" className="btn btn-dark border border-end-0">Teams</button>
-          <button type="button" className="btn btn-dark border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+          <button type="button" className="btn border border-end-0">Teams</button>
+          <button type="button" className="btn border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
           </button>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/teams">All Teams</a></li>
           </ul>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn btn-dark border border-end-0">Players</button>
-          <button type="button" className="btn btn-dark border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+          <button type="button" className="btn border border-end-0">Players</button>
+          <button type="button" className="btn border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
           </button>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/players">All Players</a></li>
           </ul>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn btn-dark border border-end-0">Games</button>
-          <button type="button" className="btn btn-dark border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+          <button type="button" className="btn btn border border-end-0">Games</button>
+          <button type="button" className="btn border border-start-0 dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
           </button>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/games">All Games</a></li>
